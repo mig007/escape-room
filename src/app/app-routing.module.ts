1491580446180eb995066a8125fda95cffafa46a
Mike Marshall <3486370+mig007@users.noreply.github.com>
2021-07-25 23:43:35 -0600

    Docs added ng build --output-path docs --base-href /your_project_name/

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TriviaComponent } from './trivia/trivia.component';
+import { HomeComponent } from './home/home.component';
+import { VideoComponent } from './video/video.component';
+import { TranslateComponent } from './translate/translate.component';
+import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'trivia', component: TriviaComponent, data: { tite: 'Trivia Game' } },
+  { path: 'video/:id', component: VideoComponent },
+  { path: 'translate', component: TranslateComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'map', component: MapComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
